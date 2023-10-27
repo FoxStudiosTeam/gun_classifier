30c2af9dcd221cdfa064c7de7bf5eee2347e4719
Kaiva-Morphin <bebraeveryday0@gmail.com>
2023-10-28 01:41:07 +0300

    updates

--- tester.py
+++ tester.py
@@ -1,21 +1,16 @@
 import cv2
-from main import *
+import numpy as np
+from main import get_model
 
-DIM =  (150,150) 
+DIM = (150,150) 
 
 model = get_model()
 model.load_weights('my_model.h5')
 
-
-data = cv2.imread("C:\\prog\\py\\nn\\Separated\\FinalImages\\NoWeapon\\303.png")
-data = cv2.resize(data, dsize =DIM, interpolation = cv2.INTER_CUBIC)
-data = np.expand_dims(data, axis=0)
-res = model.predict(x= data, batch_size= 1)
-print(res)
-
-
-data = cv2.imread("C:\\prog\\py\\nn\\0bd1cce5c59b8d2ac79a613bc71209e2.jpg")
-data = cv2.resize(data, dsize =DIM, interpolation = cv2.INTER_CUBIC)
-data = np.expand_dims(data, axis=0)
-res = model.predict(x= data, batch_size= 1)
-print(res)
+paths = ["C:\\prog\\py\\nn\\Separated\\FinalImages\\NoWeapon\\303.png"]
+for path in paths:
+    data = cv2.imread(path)
+    data = cv2.resize(data, dsize =DIM, interpolation = cv2.INTER_CUBIC)
+    data = np.expand_dims(data, axis=0)
+    res = model.predict(x= data, batch_size= 1)
+    print(f'{path}: {res}')
